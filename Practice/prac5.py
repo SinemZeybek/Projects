4a8312e3d8dbb941327fc36a0347f18015be5cce
Sinem Zeybek <sinem.zeybek8@gmail.com>
2025-05-21 10:54:10 +0200

    prac6 and texts added

---
+++ Practice/prac5.py
@@ -0,0 +1,10 @@
+def hello(name):
+    surname = None
+    if name == "Sinem":
+        print("Hello again")
+    elif name == "Furkan":
+        surname = "Özkan"
+    else:
+        print(surname)
+
+hello("Ahmet")
